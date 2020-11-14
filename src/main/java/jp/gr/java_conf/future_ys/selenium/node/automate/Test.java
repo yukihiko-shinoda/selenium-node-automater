6ebee5be049aa732e8df9141be6c02f0c9cad6f9
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2020-11-14 20:02:58 +0900

    First commit

---
+++ src/main/java/jp/gr/java_conf/future_ys/selenium/node/automate/Test.java
@@ -0,0 +1,7 @@
+package jp.gr.java_conf.future_ys.selenium.node.automate;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/jp/gr/java_conf/future_ys/selenium/node/automate/BaseTest.java
@@ -0,0 +1,21 @@
+package jp.gr.java_conf.future_ys.selenium.node.automate;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com/xhtml");
+        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
+    }
+}
